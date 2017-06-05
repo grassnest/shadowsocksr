647d9a5cfffba46491a66d423f0b32a9c844ff58
grassnest <xiaobin517@gmail.com>
2017-06-05 09:26:57 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import logging
+
+PANEL_VERSION = 'V3' # V2 or V3. V2 not support API
+
+API_URL = 'http://www.XXXXXX.com/includes/api.php'
+API_USER = 'user'
+API_PASSWORD = 'password'
+
+NODE_ID = '1'
+CHECKTIME = 15
+SYNCTIME = 300
+
+SS_BIND_IP = '0.0.0.0'
+SS_METHOD = 'rc4-md5'
+
+#LOG CONFIG
+LOG_ENABLE = True
+LOG_LEVEL = logging.DEBUG
+LOG_FILE = '/var/log/shadowsocks.log'
